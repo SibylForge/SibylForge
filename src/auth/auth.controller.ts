d8944b7e986620eec07a2a5c6927252b8a8785a0
Clooooode <clode@clo5de.info>
2023-11-24 10:05:46 +0800

    test: case for sign

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
+import { AuthSignRequest, AuthSignResponse } from './auth.dto';
+
 @Controller('auth')
 export class AuthController {
 	constructor(private readonly jwtService: JwtService) {}
 
 	@Post('sign')
-	public async sign(@Body() body: { name: string; }): Promise<any> {
+	public async sign(@Body() body: AuthSignRequest): Promise<AuthSignResponse> {
 		const { name } = body;
 
-		return this.jwtService.signAsync({
-			name,
-		});
+		return {
+			token: this.jwtService.sign({ name }),
+		};
 	}
 }
---
+++ src/auth/auth.spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+
+import { AuthController } from './auth.controller';
+import { AuthModule } from './auth.module';
+
+describe('AuthController', () => {
+	let jwtService: JwtService;
+	let authController: AuthController;
+
+	beforeAll(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [
+				ConfigModule.forRoot({ isGlobal: true }),
+				AuthModule,
+			],
+		}).compile();
+		jwtService = moduleFixture.get(JwtService);
+
+		authController = new AuthController(jwtService);
+	});
+
+	it('/sign (POST)', async () => {
+		const { token } = await authController.sign({ name: '1234' });
+		const decrypted = jwtService.decode(token);
+		expect(decrypted.name).toBe('1234');
+	});
+});
